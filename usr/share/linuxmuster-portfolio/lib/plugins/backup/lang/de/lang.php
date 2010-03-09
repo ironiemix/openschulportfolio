386e386c251cf47b15ac7204858ce32c5efda83b
fsch <fsch@813a3e44-1719-4b4c-870e-c44c740aaf87>
2010-03-09 16:22:44 +0000

    Test

---
+++ usr/share/linuxmuster-portfolio/lib/plugins/backup/lang/de/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Terence J. Grant<tjgrant@tatewake.com>
+ */
+ 
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+ 
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Backup Tool';
+
+$lang['bt_item_type'] = 'Objekttyp';
+$lang['bt_add_to_archive'] = 'Ins Backup?';
+$lang['bt_pages'] = 'Wiki-Seiten';
+$lang['bt_revisions'] = 'Alte Versionen der Wiki-Seiten';
+$lang['bt_subscriptions'] = 'Metadaten (Subscriptions)';
+$lang['bt_media'] = 'Media-Dateien';
+$lang['bt_config'] = 'Kofigurationsdaten: Wiki/<acronym title="Access Control List">ACL</acronym>/User Config';
+$lang['bt_templates'] = 'Templated';
+$lang['bt_plugins'] = 'Plugins';
+$lang['bt_create_backup'] = 'Backup erstellen';
+$lang['bt_archiving'] = 'Erstelle Backupdatei';
+$lang['bt_compressing_archive'] = 'Komprimiere Backupdatei';
+
