0f861047f2486b93d8ec177c4fac17868959ca6a
fsch <fsch@813a3e44-1719-4b4c-870e-c44c740aaf87>
2010-11-27 19:36:12 +0000

    Copied de to en

---
+++ usr/share/linuxmuster-portfolio/lib/plugins/doctree2filelist/lang/en/lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Frank Schiebel <frank@linuxmuster.net>
+ */
+
+// custom language strings for the ospdocimport plugin
+$lang['plugname'] = 'Importassistent für bestehende Dokumentensammlungen';
+$lang['headline'] = 'Importassistent für bestehende Dokumentensammlungen';
+$lang['wizard'] = 'Assistent';
+$lang['settings'] = 'Einstellungen';
+$lang['importdir'] = 'Importverzeichnis';
+$lang['targetns'] = 'Ziel-Namensraum';
+$lang['description'] = 'Dieser Assistent soll Ihnen ermöglichen, eine vorhanden Sammlung von Office-Dokumenten (Word, Powerpoint, PDF u.ä.) als Dateiverweise in das Wiki zu importieren.';
+$lang['detaildesc'] = '<p>Der Vorgang besteht aus vier Schritten:</p>
+         <ol><li>Zunächst wird der gesamte vorhandene Dokumentenbestand mit allen Unterverzeichnissen auf den Server kopiert, auf dem openSchulportftolio installiert ist.
+         Dazu wird vom Assistenten vorübergehend ein Verzeichnis angelegt, in welches die Dateien transferiert werden müssen.</li>
+         <li>In einem weiteren Schritt werden die Dateien in den eigentlichen Dokumentenbaum des Wikis kopiert. Dabei werden die Dateien wenn nötig umbenannt
+         (Umlaute, Leerzeichen und ähnliches sind für die Verwendung in Online-Systemen nicht geeignet). Außerdem wird bei diesem Vorgang im Wiki eine
+         Seitenstruktur erzeugt, über die alle kopierten Dokumente anschließend erreichbar sind. Diese Wikiseiten können nach erfolgreichem Import beliebig
+         angepasst werden.</li>
+         <li>Wenn alles geklappt hat, können die zuvor auf den Server geladenen Dokumente gelöscht werden.</li>
+         </ol>';
+$lang['warning_osp'] = 'Beim Importvorgang werden alle Dokumente und Wiki-Seiten im Ziel-Namensraum <strong>unwiderruflich durch den importierten Dokumentenstamm ersetzt</strong>!';
+$lang['filelist_plugin_required'] = 'Dieses Plugin ben&ouml;tigt das <tt>filelist</tt>-Plugin f&uuml;r DokuWiki.</div>';
+
+$lang['sourcedir_exists'] = 'Das Importverzeichnis existiert.';
+$lang['sourcedir_does_not_exist'] = 'Das Importverzeichnis existiert <strong>nicht</strong>. ';
+$lang['docuploadnow'] = "Laden Sie nun den gesamten Verzeichnisbaum ihrer Dokumentensammlung in das Importverzeichnis auf den Webserver. Wenn der Vorgang beendet ist, bestätigen Sie den Upload."; 
+$lang['importnow'] = "Starten Sie nun den Importvorgang."; 
+$lang['lastimport'] ="Letzer Import:";
+$lang['fromuser'] ="von Benutzer";
+$lang['docsuploaded'] ="Die Dokumentensammlung wurde auf den Server hochgeladen.";
+$lang['btn_import'] = 'Dateien importieren und Dokumentstruktur anlegen';
+$lang['btn_reimport'] = 'Dateien erneut importieren';
+$lang['btn_create_upload_dir'] = 'Importverzeichnis anlegen';
+$lang['btn_delete_upload_dir'] = 'Importverzeichnis löschen';
+$lang['btn_confirm_upload'] = 'Die Dateien wurden hochgeladen';
+$lang['btn_start_over'] = 'Import neu starten';
---
+++ usr/share/linuxmuster-portfolio/lib/plugins/doctree2filelist/lang/en/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Frank Schiebel <frank@linuxmuster.net>
+ */
+ 
+// for the configuration manager
+$lang['sourcetree']            = 'Uploadverzeichnis, aus dem der Dokumentstamm in das Wiki kopiert wird.';
+$lang['destination_namespace'] = "Namensraum, in dem die Wikiseiten für die Dokumente generiert werden.";
+
