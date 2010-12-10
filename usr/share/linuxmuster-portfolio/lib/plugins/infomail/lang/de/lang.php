f8478b85c169f068a88ccd008da53a6ae6c848eb
fsch <fsch@813a3e44-1719-4b4c-870e-c44c740aaf87>
2010-12-10 11:55:47 +0000

    en lang

---
+++ usr/share/linuxmuster-portfolio/lib/plugins/infomail/lang/de/lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['formname']= "Infomail:";
+$lang['yourname']= "Ihr Name:";
+$lang['youremailaddress']= "Ihre Mail-Adresse:";
+$lang['noneselected'] = "Keines gewählt...";
+$lang['bookmarks'] = "Lesezeichen:";
+$lang['more_rec_fill'] = "Weitere ";
+$lang['recipients'] = "Empfänger:";
+$lang['subject'] = "Betreff:";
+$lang['message'] = "Nachricht:";
+$lang['send_infomail'] = "Mail senden";
+$lang['cancel_infomail'] = "Abbrechen";
+$lang['novalid_rec'] = "Sie haben keine gültigen Empfänger angegeben!";
+$lang['invalid_sender'] = "Sie müssen einen Absender angeben!";
+$lang['invalid_pageid'] = "Sie haben keinen gültigen Seitennamen angegeben!";
