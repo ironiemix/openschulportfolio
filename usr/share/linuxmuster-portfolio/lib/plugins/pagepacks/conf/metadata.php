1802a60573fdc899cb390da77b1ae9ec8c3b5dd9
root <root@linuxmuster.lokal>
2012-07-02 10:53:18 +0200

    Parallelzweig mit pagepacks aber altem DokuWiki

---
+++ usr/share/linuxmuster-portfolio/lib/plugins/pagepacks/conf/metadata.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the pagepacks plugin
+ *
+ * @author Frank Schiebel <frank@linuxmuster.net>
+ */
+
+$meta['pagepacks_src'] = array('');
---
+++ usr/share/linuxmuster-portfolio/lib/plugins/pagepacks/lang/de/lang.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * additional language strings used by plugin
+ *
+ * german version
+ *
+ * @author Frank Schiebel <frank@linuxmuster.net>
+ */
+
+$lang['pagepacks'] = "Verfügbare Seitenpakete";
+$lang['btn_unzip'] = "Auspacken";
+
